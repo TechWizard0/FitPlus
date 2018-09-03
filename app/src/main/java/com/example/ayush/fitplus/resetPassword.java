182133a54514f24bcb339c874a599e85ceaf4bfa
ayushbathrey <ayushbathrey3@gmail.com>
2018-09-03 12:44:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ayush/fitplus/resetPassword.java
@@ -0,0 +1,13 @@
+package com.example.ayush.fitplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class resetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
